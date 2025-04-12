c80882ce1be81e53346db3707c1a87f2688fb02c
mahmud-devs <mahmudulislam0999@gmail.com>
2025-04-12 22:08:36 +0600

    '

--- app/about.jsx
+++ app/about.jsx
@@ -7,7 +7,7 @@ const about = () => {
         <View className="flex-1 justify-center items-center bg-black">
             <TouchableOpacity>
                 <Text className="bg-red-500 px-10 py-10 text-white rounded-lg text-[25px]">
-                    Press Here
+                    Press Here to
                 </Text>
             </TouchableOpacity>
         </View>
